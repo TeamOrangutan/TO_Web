5ccef765bb82973f75a79874dc65ab6184691665
Yasser <yassercomunica@gmail.com>
2025-06-27 21:13:03 -0600

    Responsive carrito

--- teamOrangutan/src/router/user/PrivateRoute.jsx
+++ teamOrangutan/src/router/user/PrivateRoute.jsx
@@ -3,11 +3,14 @@ import { AuthContext } from "../../Auth/user/context/AuthContext";
 import { Navigate } from "react-router-dom";
 
 export const PrivateRoute = ({ children }) => {
-  const { isAuthenticated, user } = useContext(AuthContext);
+  const { isAuthenticated, user, rol } = useContext(AuthContext);
 
 console.log("user");
-console.log(user);
+console.log(rol);
 
+if (!isAuthenticated) {
+    return <Navigate to="/" />;
+  }
 
   return isAuthenticated ? children : <Navigate to="/" />;
 };
